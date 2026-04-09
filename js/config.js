a214907a5430dd1f1e250fddd47d2b5062059ebc
jordoncwk <jordoncwk@users.noreply.github.com>
2026-04-09 10:06:59 +0800

    fix: update GAS_URL to new deployment with public access

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Replace with your deployed GAS web app URL after Task 14
-export const GAS_URL = 'https://script.google.com/macros/s/AKfycbxtlCj6mAO-DYrMDGWSfIqIecX3nsCTSU2OHsL-WWVSHYsFu4p-ajLgnr16-QTXOvQW/exec';
+export const GAS_URL = 'https://script.google.com/macros/s/AKfycbzTLfGH1FmUH2unbAZgAH_vi-SSmTGoPGobdFtX6p8APwLcyjAdzs_JZj9WQMJdhNer/exec';
