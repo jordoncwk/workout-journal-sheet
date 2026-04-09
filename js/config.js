7e050c3b623a8a3adedcdfeda388367b9fa40476
jordoncwk <jordoncwk@users.noreply.github.com>
2026-04-09 10:00:21 +0800

    feat: set GAS_URL for deployed web app

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Replace with your deployed GAS web app URL after Task 14
-export const GAS_URL = '';
+export const GAS_URL = 'https://script.google.com/macros/s/AKfycbxtlCj6mAO-DYrMDGWSfIqIecX3nsCTSU2OHsL-WWVSHYsFu4p-ajLgnr16-QTXOvQW/exec';
